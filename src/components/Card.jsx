d6d9a4bfeb4a8b921ed10aded371cedc60cb712f
Julien RICHARD <julien.y.richard@gmail.com>
2023-12-14 10:30:07 +0100

    Ajout des porpstypes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,35 @@
-import { useState } from 'react';
-import { FaRegStar, FaTrash } from 'react-icons/fa';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { FaRegStar, FaTrash } from "react-icons/fa";
 
 const Card = ({ task }) => {
   const [like, setLike] = useState(false);
 
-  console.log(like)
   return (
     <div className="card">
-      <h3>Titre : {task.title }</h3>
-      <h5>Description de la tache: <span>{task.description }</span></h5>
-      <h6>Status: {task.status }</h6>
-      <p>Date butoire: {task.date }</p>
+      <h3>Titre : {task.title}</h3>
+      <h5>
+        Description de la tache: <span>{task.description}</span>
+      </h5>
+      <h6>Status: {task.status}</h6>
+      <p>Date butoire: {task.date}</p>
       <div className="stars" onClick={() => setLike(!like)}>
-        <FaRegStar fill={like ? "yellow" : "black" } />
+        <FaRegStar fill={like ? "yellow" : "black"} />
       </div>
       <div className="trash">
         <FaTrash />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+Card.propTypes = {
+  task: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Card;
